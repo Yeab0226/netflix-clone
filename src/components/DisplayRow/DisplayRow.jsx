1a5cc84e14793673b5e52e3e95089890ef94627e
temesgenazmeraw-stack <temesgen.azmeraw@aau.edu.et>
2026-08-27 21:40:24 +0300

    Display Row is Updated

--- src/components/DisplayRow/DisplayRow.jsx
+++ src/components/DisplayRow/DisplayRow.jsx
@@ -1,14 +1,70 @@
+import React, { useEffect, useState } from "react";
 import styles from "./DisplayRow.module.css";
-import SlideShow from "../SlideShow/SlideShow";
+import SlideShow from "./../SlideShow/SlideShow";
+// import { movies } from "../../Data/Data";
+import { movieInstance } from "../../Utility/MovieInstance";
+import requests from "../../Utility/requestUrls";
 
 function DisplayRow() {
-  // T-12: Fetch and store the movie categories here.
+  const [movies, setMovies] = useState({
+    trending: [],
+    netflixOriginals: [],
+    topRated: [],
+    action: [],
+    comedy: [],
+    horror: [],
+    romance: [],
+    documentaries: [],
+  });
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async () => {
+    try {
+      const [
+        trendingRes,
+        netflixRes,
+        topRatedRes,
+        actionRes,
+        comedyRes,
+        horrorRes,
+        romanceRes,
+        docRes,
+      ] = await Promise.all([
+        movieInstance.get(requests.fetchTrending),
+        movieInstance.get(requests.fetchNetflixOriginals),
+        movieInstance.get(requests.fetchTopRatedMovies),
+        movieInstance.get(requests.fetchActionMovies),
+        movieInstance.get(requests.fetchComedyMovies),
+        movieInstance.get(requests.fetchHorrorMovies),
+        movieInstance.get(requests.fetchRomanceMovies),
+        movieInstance.get(requests.fetchDocumentaries),
+      ]);
+
+      setMovies({
+        trending: trendingRes.data.results,
+        netflixOriginals: netflixRes.data.results,
+        topRated: topRatedRes.data.results,
+        action: actionRes.data.results,
+        comedy: comedyRes.data.results,
+        horror: horrorRes.data.results,
+        romance: romanceRes.data.results,
+        documentaries: docRes.data.results,
+      });
+    } catch (error) {
+      console.error("unable to fetch movies", error);
+    }
+  };
 
   return (
-    <main className={styles.mainWrapper}>
-      {/* T-12/T-13: Add movie rows here. */}
-      <SlideShow title="Trending Movies" movies={[]} />
-    </main>
+    <div className={styles.mainWrapper}>
+      <SlideShow title="Trending Movies" movies={movies.trending} />
+      <SlideShow title="Popular on Netflix" movies={movies.netflixOriginals} />
+      <SlideShow title="Action Movies" movies={movies.action} />
+      <SlideShow title="Top Rated" movies={movies.topRated} />
+    </div>
   );
 }
 
