238c3873d296f20c7b759571f95616e6bdc16464
zeleke21977 <zolakid50@gmail.com>
2026-08-27 10:33:53 +0300

    Add feature B

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,30 @@ function Header() {
   return (
     <header className={styles.header}>
       {/* T-07/T-08: Build the Netflix header here. */}
+
+      <div className={Styles.rightSection}>
+        {/* search and notification icon  */}
+        <div className={Styles.searchContainer}>
+          <button
+            onClick={() => setIsSearchOpen(!isSearchOpen)}
+            className={Styles.searchButton}
+          >
+            <Search size={20} />
+          </button>
+
+          {isSearchOpen && (
+            <input
+              type="text"
+              placeholder="movie title"
+              className={Styles.searchInput}
+            />
+          )}
+        </div>
+        <button className={Styles.iconButton}>
+          <Bell size={20} />
+          <span className={Styles.notificationBadge}>4</span>
+        </button>
+      </div>
     </header>
   );
 }
